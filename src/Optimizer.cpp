2be03796426d227dbf5138f9b08fe6524ab9f965
hansry <1057064956@qq.com>
2020-03-19 00:39:14 +0800

    modify code in optimizer.cpp

--- src/Optimizer.cpp
+++ src/Optimizer.cpp
@@ -788,7 +788,7 @@ int Optimizer::PoseOptimization_icp(Frame* pFrame, cv::Mat& curr_depth, cv::Mat&
 	      edge->setId( icp_index );
 	      edge->setVertex(0, dynamic_cast<g2o::VertexSE3Expmap*>(optimizer.vertex(0)));
 	      edge->setMeasurement(Eigen::Vector3d(temp_curr_point.at<float>(0,0), temp_curr_point.at<float>(1,0), temp_curr_point.at<float>(2,0)));//点1=T*点2=点1'
-	      edge->setInformation( Eigen::Matrix3d::Identity()*40 );//误差项系数矩阵  信息矩阵
+	      edge->setInformation( Eigen::Matrix3d::Identity());//误差项系数矩阵  信息矩阵
 	     
  	      g2o::RobustKernelHuber* rk_icp = new g2o::RobustKernelHuber;
  	      edge->setRobustKernel(rk_icp);
